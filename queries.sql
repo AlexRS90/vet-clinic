8ebee7228a6fdc93e2104fe814909323b62e1146
AlexRS90 <aramosbengy@gmail.com>
2022-01-12 15:41:32 -0600

    Write queries to answer questions

--- queries.sql
+++ queries.sql
@@ -44,3 +44,11 @@ SELECT AVG (weight_kg) FROM animals;
 SELECT neutered, SUM (escape_attempts) FROM animals GROUP BY neutered;
 SELECT species, MIN (weight_kg), MAX (weight_kg) FROM animals GROUP BY species;
 SELECT species, AVG (escape_attempts) FROM animals WHERE date_of_birth >= '01-01-1990'::date AND date_of_birth <= '12-31-2000'::date GROUP BY species;
+
+SELECT animal_name FROM animals JOIN owners ON animals.owner_id = owners.id WHERE owners.full_name = 'Melody Pond';
+SELECT animal_name FROM animals JOIN species ON animals.species_id = species.id WHERE species.name = 'Pokemon';
+SELECT animal_name, full_name FROM animals FULL OUTER JOIN owners ON animals.owner_id = owners.id;
+SELECT name, COUNT(species_id) FROM species JOIN animals ON animals.species_id = species.id GROUP BY species.name;
+SELECT animal_name FROM animals JOIN owners ON animals.owner_id = owners.id JOIN species ON animals.species_id = species.id WHERE owners.full_name = 'Jennifer Orwell' AND species.name = 'Digimon';
+SELECT full_name, animal_name, escape_attempts FROM animals JOIN owners ON animals.owner_id = owners.id WHERE owners.full_name = 'Dean Winchester' AND escape_attempts = 0;
+SELECT full_name, COUNT (full_name) AS number_of_animals from owners JOIN animals ON animals.owner_id = owners.id GROUP BY owners.full_name ORDER BY number_of_animals DESC LIMIT 1;
